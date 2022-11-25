b13a816cdab5150c183ca6a07dbb3941a8b5c4dc
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-11-25 20:32:16 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.innerHTML = galleryMarkup;
+
+galleryRef.addEventListener("click", onImageClick);
+
+galleryRef.addEventListener("keydown", onEscClick);
+
+
+function createGalleryMarkup(array) {
+    
+    return array.map(({preview, original, description}) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join("");
+}
+
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+
+    galleryRef.addEventListener("keydown", onEscClick);
+
+    function onEscClick(evt) {
+        if (evt.code === "Escape") {
+            instance.close()
+        }
+    }
+}
